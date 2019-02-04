c411009a922b89e61ec7402403f3cad70e4ba14e
Vsevolodnest <34964944+Vsevolodnest@users.noreply.github.com>
2019-02-04 19:50:45 +0300

    pf4j dummyplugins 1 and 2
    pf4j framework is integrated into the current version of inception-plugin-manager. In the specified directory, a list of jar plugins is defined, after that they are run in the system

---
+++ inception-pluginserver-webapp/src/main/java/de/tudarmstadt/ukp/inception/pluginserver/SpringConfiguration.java
@@ -0,0 +1,28 @@
+package de.tudarmstadt.ukp.inception.pluginserver;
+
+import org.pf4j.demo.api.Greeting;
+import org.pf4j.spring.SpringPluginManager;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
+
+import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class SpringConfiguration {
+	@Bean
+    public SpringPluginManager pluginManager() {
+		/*
+		 * need specify the correct path 
+		 */
+        return new SpringPluginManager(Paths.get("D:\\plugins"));
+    }
+
+    @Bean
+    @DependsOn("pluginManager")
+    public Greetings greetings() {
+        return new Greetings();
+    }
+}
